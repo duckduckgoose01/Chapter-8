e6e1adf2967fd5ae7c02b0766f39d64aab30ed5d
duckduckgoose01 <davidshapiro@brandeis.edu>
2022-10-09 17:15:09 -0400

    one excerise

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,14 +1,20 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
   
   def setup
     @user = users(:michael)
   end
+end
 
-  test "login with valid email/invalid password" do
+class InvalidPasswordTest < UsersLogin
+
+  test "login path" do
     get login_path
     assert_template 'sessions/new'
+  end
+
+  test "login with valid email/invalid password" do
     post login_path, params: { session: { email:    @user.email,
                                           password: "invalid" } }
     assert_not is_logged_in?
@@ -18,21 +24,50 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+end
 
-  test "login with valid information followed by logout" do
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
+class ValidLogin < UsersLogin
+
+  def setup
+    super
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password'}}
+  end
+end
+
+class ValidLoginTest < ValidLogin
+
+  test "valid login" do
     assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+  end
+end
+
+class Logout < ValidLogin
+
+  def setup
+    super
     delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+
+  test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
+  end
+
+  test "redirect after logout" do
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
